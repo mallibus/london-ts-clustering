cca5c77a3e250e206d998c5ea30feab83451d35c
Marcello Morchio <marcello@datascienceseed.com>
2021-12-28 17:03:35 +0100

    experiments with tsfel

---
+++ load_full.bash
@@ -0,0 +1,3 @@
+# Full dataset load
+wget https://data.london.gov.uk/download/smartmeter-energy-use-data-in-london-households/3527bf39-d93e-4071-8451-df2ade1ea4f2/LCL-FullData.zip
+unzip LCL-FullData.zip
---
+++ load_partitioned.bash
@@ -0,0 +1,3 @@
+# Full dataset load
+wget https://data.london.gov.uk/download/smartmeter-energy-use-data-in-london-households/04feba67-f1a3-4563-98d0-f3071e3d56d1/Partitioned%20LCL%20Data.zip
+unzip 'Partitioned%20LCL Data.zip'
